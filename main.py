6c2a285fff9d0dd6330ce2a4c200c18649c5de2e
moeinovic <76170229+moeinovic@users.noreply.github.com>
2021-01-02 17:57:59 +0330

    Edit fucking int

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from pyrogram import Client
 plugins = dict(root="plugins")
-API_ID = int(os.environ.get('APP_ID'))
+API_ID = os.environ.get('APP_ID')
 API_HASH = os.environ.get('APP_HASH')
 TOKEN = os.environ.get('TOKEN')
 Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN, plugins=plugins).run()
