833f6e2b8834e884708e8ccc13b6a55e5c070160
moeinovic <76170229+moeinovic@users.noreply.github.com>
2021-01-02 18:23:56 +0330

    Update handler.py

--- plugins/handler.py
+++ plugins/handler.py
@@ -2,4 +2,4 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command("start") & filters.private)
 def echo(client, message):
-    message.reply("Hi Men)
+    message.reply("Hi Men")
