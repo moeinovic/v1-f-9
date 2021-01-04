f57adc8a4851e2c78fa2b9af4aac93a40b0875ba
moeinovic <76170229+moeinovic@users.noreply.github.com>
2021-01-04 09:43:28 +0330

    Update handler.py

--- plugins/handler.py
+++ plugins/handler.py
@@ -1,8 +1,16 @@
 from pyrogram import Client, filters
-
+from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent
 @Client.on_message(filters.command("start") & filters.private)
 def echo(client, message):
     message.reply("Hi Men")
-@Client.on_inline_query(filters.user('moeinovic'))
-def inline(c,m):
-    client.send_message('moeinovic',m)
+@Client.on_inline_query()
+async def inlienQuery(c,q):
+  await c.answer_inline_query(q.id, 
+  results=[
+   InlineQueryResultArticle(
+      'your title',
+      InputTextMessageContent('your text content'),
+      description='inline query description'
+    )
+  ]
+)
