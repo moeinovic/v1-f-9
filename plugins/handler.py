1e4a1b7e20d501452a1e1c4e6472b029e3083cd1
moeinovic <76170229+moeinovic@users.noreply.github.com>
2021-01-04 08:57:56 +0330

    Update handler.py

--- plugins/handler.py
+++ plugins/handler.py
@@ -3,6 +3,6 @@ from pyrogram import Client, filters
 @Client.on_message(filters.command("start") & filters.private)
 def echo(client, message):
     message.reply("Hi Men")
-@Client.on_inline_query(filters.users('moeinovic'))
+@Client.on_inline_query(filters.user('moeinovic'))
 def inline(c,m):
     client.send_message('moeinovic',m)
