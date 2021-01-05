8054bedf9c49ca10e4e3981ce7388f8de14a2b07
moeinovic <76170229+moeinovic@users.noreply.github.com>
2021-01-05 19:28:07 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import os
-from pyrogram import Client
+from pyrogram import Client, filters
+from pyromod import listen
 plugins = dict(root="plugins")
 API_ID = int(os.environ.get('APIID'))
 API_HASH = os.environ.get('APIHASH')
 TOKEN = os.environ.get('TOKEN')
-Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN, plugins=plugins).run()
+app = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+@app.on_message(filters.command("start") & filters.private)
+async def echo(c, m):
+    answer = await app.ask(m.chat.id, '*Send me your name:*', parse_mode='Markdown')
+    await app.send_message(m.chat.id, f'Your name is: {answer.text}')
+
+app.run()
