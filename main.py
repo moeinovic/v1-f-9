7f3cbc5f414859b5075112899a78dc1283fab2ab
moeinovic <76170229+moeinovic@users.noreply.github.com>
2021-01-05 19:49:53 +0330

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ TOKEN = os.environ.get('TOKEN')
 app = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 @app.on_message(filters.command("start") & filters.private)
 async def echo(c, m):
-    answer = await app.ask(m.chat.id, '*Send me your name:*', parse_mode='Markdown')
-    await app.send_message(m.chat.id, f'Your name is: {answer.text}')
+    answer = await app.listen(m.chat.id, timeout=30)
+    await app.send_message(m.chat.id, answer.text)
 
 app.run()
