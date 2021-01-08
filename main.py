242dc8b744a75f8cf9a11d37748b6f1736d2c06b
moeinovic <76170229+moeinovic@users.noreply.github.com>
2021-01-08 20:17:08 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,4 @@ plugins = dict(root="plugins")
 API_ID = int(os.environ.get('APIID'))
 API_HASH = os.environ.get('APIHASH')
 TOKEN = os.environ.get('TOKEN')
-app = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
-@app.on_message(filters.command("start") & filters.private)
-async def echo(c, m):
-    answer = await app.listen(m.chat.id, timeout=30)
-    await app.send_message(m.chat.id, answer.text)
-
-app.run()
+Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN, plugins=plugins).run()
