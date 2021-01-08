f904fbe54e81ea30de0f6e71f64ca7567a0a9488
moeinovic <76170229+moeinovic@users.noreply.github.com>
2021-01-08 20:15:01 +0330

    Update handler.py

--- plugins/handler.py
+++ plugins/handler.py
@@ -1,7 +1,5 @@
 from pyrogram import Client, filters
-from pyromod import listen
 from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardMarkup, InlineKeyboardButton
 @Client.on_message(filters.command("start") & filters.private)
 async def echo(c, m):
-    answer = await c.ask(m.chat.id, '*Send me your name:*', parse_mode='Markdown')
-    await c.send_message(m.chat.id, f'Your name is: {answer.text}')
+	await c.send_message(m.chat.id, '*Hello My Friend*', parse_mode="MarkDown")
